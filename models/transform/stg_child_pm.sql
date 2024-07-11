da5cd6d18d8e57b232d20b2aebe1e950b56715d8
Pravalika Thadaka <pravalikasatyanarayana@gmail.com>
2024-07-11 14:43:55 +0000

    pm changes made

--- models/transform/stg_child_pm.sql
+++ models/transform/stg_child_pm.sql
@@ -9,17 +9,22 @@
 }}
 
 
-with task_count_tab as (
-    select two.TEMPLATE_WO_ID as TEMPLATE_WO_ID,count(distinct( TEMPLATE_WO_TASK_ID)) as task_count  
-FROM
-{{source('spear','TA_TEMPLATE_WO_TASK')}} twt JOIN
-{{source('spear','TA_TEMPLATE_WO')}} two on two.TEMPLATE_WO_ID= twt.TEMPLATE_WO_ID 
-group by two.TEMPLATE_WO_ID
+with two_count_tab as (
+select pm.MAINT_PATTERN_CODE,e.EQUIP_CODE,count(distinct(two.TEMPLATE_WO_ID)) as two_count
+from
+     {{ source('spear','TA_WO_INFORMATION')}}  woi 
+    JOIN {{ source('spear','TA_EQUIPMENT')}}  e               ON e.EQUIP_ID = woi.EQUIP_ID                          
+    JOIN {{ source('spear','TA_PATTERN_ELEMENT')}} pe        ON woi.PATTERN_ELEM_ID=pe.PATTERN_ELEM_ID and woi.MAINT_PATTERN_CODE=pe.MAINT_PATTERN_CODE
+    JOIN {{ source('spear','TA_MAINT_PATTERN')}}  pm          ON pe.MAINT_PATTERN_CODE=pm.MAINT_PATTERN_CODE
+    LEFT JOIN {{ source('spear','TA_TEMPLATE_WO')}}  two      ON woi.TEMPLATE_WO_ID=two.TEMPLATE_WO_ID
+   
+    group by pm.MAINT_PATTERN_CODE,e.EQUIP_CODE
+    order by pm.MAINT_PATTERN_CODE,e.EQUIP_CODE
  )
 
 Select 
 
-pm.MAINT_PATTERN_CODE|| '-'||e.EQUIP_CODE|| '-'||pe.PATTERN_ELEM_ORDER as PMNUM,
+concat(trim(pm.MAINT_PATTERN_CODE),'-',trim(e.EQUIP_CODE),'-',trim(pe.PATTERN_ELEM_ORDER))as PMNUM,
 two.WO_TITLE as DESCRIPTION,
 'NRPC' as ORGID,
 'AMTRAK' as SITEID,
@@ -28,7 +33,7 @@ e.EQUIP_CODE as LOCATION,
 1 as LEADTIMEACTIVE,
 two.WO_TYPE_CODE as WORKTYPE,
 'REVIEWED' as WOSTATUS,
-'AG_'||twt.TEMPLATE_WO_ID as JPNUM,
+'AG_'||two.TEMPLATE_WO_ID as JPNUM,
 'ME' as OWNERGROUP,
 'AMTRAK' as STORELOCSITE,
 pe.RECUR_INTERVAL_NBR as FREQUENCY,
@@ -39,7 +44,9 @@ CASE
 as FREQUNIT,
 'SEQUENTIAL_PATTERN' as BRDAPMSEQTYPE,
 null  as BRDAHIERSEQINUSE,
-pe.PATTERN_ELEM_ORDER as WOSEQUENCE
+pe.PATTERN_ELEM_ORDER as WOSEQUENCE,
+pm.MAINT_PATTERN_CODE|| '-'||e.EQUIP_CODE as PARENT
+
 
 from 
          {{ source('spear','TA_WO_INFORMATION')}}  woi 
@@ -47,9 +54,7 @@ from
     JOIN {{ source('spear','TA_PATTERN_ELEMENT')}} pe        ON woi.PATTERN_ELEM_ID=pe.PATTERN_ELEM_ID and woi.MAINT_PATTERN_CODE=pe.MAINT_PATTERN_CODE
     JOIN {{ source('spear','TA_MAINT_PATTERN')}}  pm          ON pe.MAINT_PATTERN_CODE=pm.MAINT_PATTERN_CODE
     LEFT JOIN {{ source('spear','TA_TEMPLATE_WO')}}  two      ON woi.TEMPLATE_WO_ID=two.TEMPLATE_WO_ID
-    LEFT JOIN {{ source('spear','TA_TEMPLATE_WO_TASK')}}  twt ON two.TEMPLATE_WO_ID = twt.TEMPLATE_WO_ID
-    LEFT JOIN task_count_tab          ON two.TEMPLATE_WO_ID=task_count_tab.TEMPLATE_WO_ID
+    LEFT JOIN two_count_tab          ON pm.MAINT_PATTERN_CODE=two_count_tab.MAINT_PATTERN_CODE and e.EQUIP_CODE=two_count_tab.EQUIP_CODE
     where    
- 
-    task_count_tab.task_count>1
+    two_count_tab.two_count>1
 
--- models/transform/stg_parent_pm.sql
+++ models/transform/stg_parent_pm.sql
@@ -8,17 +8,22 @@
     )
 }}
 
-with task_count_tab as (
-    select two.TEMPLATE_WO_ID as TEMPLATE_WO_ID,count(distinct( TEMPLATE_WO_TASK_ID)) as task_count  
-FROM
-{{source('spear','TA_TEMPLATE_WO_TASK')}} twt JOIN
-{{source('spear','TA_TEMPLATE_WO')}} two on two.TEMPLATE_WO_ID= twt.TEMPLATE_WO_ID 
-group by two.TEMPLATE_WO_ID
+with two_count_tab as (
+select pm.MAINT_PATTERN_CODE,e.EQUIP_CODE,count(distinct(two.TEMPLATE_WO_ID)) as two_count
+from
+     {{ source('spear','TA_WO_INFORMATION')}}  woi 
+    JOIN {{ source('spear','TA_EQUIPMENT')}}  e               ON e.EQUIP_ID = woi.EQUIP_ID                          
+    JOIN {{ source('spear','TA_PATTERN_ELEMENT')}} pe        ON woi.PATTERN_ELEM_ID=pe.PATTERN_ELEM_ID and woi.MAINT_PATTERN_CODE=pe.MAINT_PATTERN_CODE
+    JOIN {{ source('spear','TA_MAINT_PATTERN')}}  pm          ON pe.MAINT_PATTERN_CODE=pm.MAINT_PATTERN_CODE
+    LEFT JOIN {{ source('spear','TA_TEMPLATE_WO')}}  two      ON woi.TEMPLATE_WO_ID=two.TEMPLATE_WO_ID
+   
+    group by pm.MAINT_PATTERN_CODE,e.EQUIP_CODE
+    order by pm.MAINT_PATTERN_CODE,e.EQUIP_CODE
  )
 
 
 Select 
-pm.MAINT_PATTERN_CODE || '-'|| e.EQUIP_CODE as PMNUM,
+trim(pm.MAINT_PATTERN_CODE)||'-'||trim(e.EQUIP_CODE) as PMNUM,
 pm.MAINT_PATTERN_DESC as DESCRIPTION,
 'NRPC' as ORGID,
 'AMTRAK' as SITEID,
@@ -29,8 +34,8 @@ e.EQUIP_CODE as LOCATION,
 two.WO_TYPE_CODE as WORKTYPE,
 'REVIEWED' as WOSTATUS,
 CASE
-    WHEN task_count_tab.task_count=1 THEN 'AG_'||twt.TEMPLATE_WO_ID
-    WHEN task_count_tab.task_count>1 THEN null
+    WHEN  two_count_tab.two_count=1 THEN 'AG_'||two.TEMPLATE_WO_ID
+    WHEN  two_count_tab.two_count>1 THEN null
     END as JPNUM,
 'ME' as OWNERGROUP,
 'AMTRAK' as STORELOCSITE,
@@ -41,15 +46,16 @@ CASE
     END 
 as FREQUNIT,
 CASE
-    WHEN task_count_tab.task_count>1 THEN 'SEQUENTIAL_PATTERN'
-    WHEN task_count_tab.task_count=1 THEN null
+    WHEN  two_count_tab.two_count>1 THEN 'SEQUENTIAL_PATTERN'
+    WHEN  two_count_tab.two_count=1 THEN null
     END  as BRDAPMSEQTYPE,
 
     CASE
-    WHEN task_count_tab.task_count>1 THEN 1
-    WHEN task_count_tab.task_count=1 THEN null
+    WHEN  two_count_tab.two_count>1 THEN 1
+    WHEN  two_count_tab.two_count=1 THEN null
     END  as BRDAHIERSEQINUSE,
-    null as WOSEQUENCE
+    null as WOSEQUENCE,
+    null as PARENT
        
 from 
          {{ source('spear','TA_WO_INFORMATION')}}  woi 
@@ -57,5 +63,6 @@ from
     JOIN {{ source('spear','TA_PATTERN_ELEMENT')}} pe        ON woi.PATTERN_ELEM_ID=pe.PATTERN_ELEM_ID and woi.MAINT_PATTERN_CODE=pe.MAINT_PATTERN_CODE
     JOIN {{ source('spear','TA_MAINT_PATTERN')}}  pm          ON pe.MAINT_PATTERN_CODE=pm.MAINT_PATTERN_CODE
     LEFT JOIN {{ source('spear','TA_TEMPLATE_WO')}}  two      ON woi.TEMPLATE_WO_ID=two.TEMPLATE_WO_ID
-    LEFT JOIN {{ source('spear','TA_TEMPLATE_WO_TASK')}}  twt ON two.TEMPLATE_WO_ID = twt.TEMPLATE_WO_ID
-    LEFT JOIN task_count_tab          ON two.TEMPLATE_WO_ID=task_count_tab.TEMPLATE_WO_ID
+   LEFT JOIN two_count_tab          ON pm.MAINT_PATTERN_CODE=two_count_tab.MAINT_PATTERN_CODE and e.EQUIP_CODE=two_count_tab.EQUIP_CODE
+    where    
+    two_count_tab.two_count>1
